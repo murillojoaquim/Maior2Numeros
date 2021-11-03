06ea07d30f38e21f33cc6826736803ecfd2f24dd
murillojoaquim <murillojalves@hotmail.com>
2021-11-03 19:41:46 -0300

    Maior2Numeros

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine($"O maior número é {n1}");
+            }
+            else
+            {
+                Console.WriteLine($"O maior número é {n2}");
+            }
+
+        }
+    }
+}
